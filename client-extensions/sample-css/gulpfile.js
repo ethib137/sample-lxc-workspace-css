8be968073d85b37009616c88581db5104a8b829f
Evan Thibodeau <evan.thibodeau@liferay.com>
2022-09-21 16:27:42 +0400

    Update build script

--- client-extensions/sample-css/gulpfile.js
+++ client-extensions/sample-css/gulpfile.js
@@ -18,9 +18,9 @@ uswds.paths.dist.css = './src';
 uswds.paths.dist.js = './src';
 uswds.paths.dist.sass = './sass';
 
-uswds.paths.src.uswds = './node_modules/@uswds';
-uswds.paths.src.sass = './node_modules/@uswds/uswds/packages';
-uswds.paths.src.theme = './node_modules/@uswds/uswds/dist/theme';
+uswds.paths.src.uswds = '../../node_modules/@uswds';
+uswds.paths.src.sass = '../../node_modules/@uswds/uswds/packages';
+uswds.paths.src.theme = '../../node_modules/@uswds/uswds/dist/theme';
 
 /**
  * Exports
